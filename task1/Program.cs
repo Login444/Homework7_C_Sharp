4bb950aca1518842522900ded17b120b4393f8a4
login4 <birukovartemij@gmail.com>
2023-01-25 21:55:37 +0300

    task1 completed

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(double[,] matr)
+{
+    for (int rows=0; rows<matr.GetLength(0); rows++)
+    {
+        for(int columns =0; columns<matr.GetLength(1); columns++)
+        {
+            matr[rows, columns] = new Random().NextDouble() * new Random().Next(10, 1000);
+        }
+    Console.WriteLine();
+    }
+}
+
+void PrintArray(double[,] matr)
+{
+    for (int rows=0; rows<matr.GetLength(0); rows++)
+    {
+        for(int columns =0; columns<matr.GetLength(1); columns++)
+        {
+            Console.Write($"{matr[rows, columns]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+double [,] arr = new double [3,4];
+
+FillArray(arr);
+PrintArray(arr);
